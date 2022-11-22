0c72c4bb445b57094ab2fc2826e51a0458a71ccd
Cory Metcalf <cmetcalf8@gmail.com>
2022-11-21 19:43:33 -0700

    updated models

--- server/models/Fav.js
+++ server/models/Fav.js
@@ -1,14 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, Types } = require('mongoose');
 
 const favSchema = new Schema(
-    {
-        favName: {
-            type: String,
-            required: true,
-        }
-    }
+  {
+    favId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    favTitle: {
+      type: String,
+      required: true,
+      maxlength: 50,
+      minlength: 4,
+      default: 'Unnamed favorite',
+    },
+    score: {
+      type: Number,
+      required: true,
+      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
-const Fav = model('fav', favSchema);
-
-module.exports = Fav;
+module.exports = favSchema;
