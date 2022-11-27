3fe78fcd58d894fcdd2ccf05c529846bd7205bbf
Cory Metcalf <cmetcalf8@gmail.com>
2022-11-26 22:02:18 -0700

    added a top 100 api to fetch on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,24 @@ import React from 'react';
 import "../App.css";
 import { MDBBtn } from 'mdb-react-ui-kit';
 
-function homePage() {
-    const handleClick = () => {
+const homePage = () => {
 
-    }
-    return (
-        <div className='home'>
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '803769a760msh0b927317a3037b5p16a8f3jsnaab08108dfb1',
+            'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://imdb-top-100-movies.p.rapidapi.com/premiummovies', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+        
+        return (
+            <div className='home'>
             <div>
                 <h1>Welcome to Streaming Roulette</h1>
             </div>
@@ -15,7 +27,7 @@ function homePage() {
                 <h4>Hit the start button below to get a random movie to watch.</h4>
             </div>
             <div>
-                <MDBBtn rounded className='text-light' color='dark' onClick={handleClick}>
+                <MDBBtn rounded className='text-light' color='dark' onClick={options}>
                     Start
                 </MDBBtn>
             </div>
